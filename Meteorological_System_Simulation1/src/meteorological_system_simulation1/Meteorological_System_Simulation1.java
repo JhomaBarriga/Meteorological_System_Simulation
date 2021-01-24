e7d34df15558a92888e3192ef6f1d4a2725fc66f
FerBacuy <dia_nara15@hotmail.com>
2021-01-23 22:53:26 -0500

    First part

---
+++ Meteorological_System_Simulation1/src/meteorological_system_simulation1/Meteorological_System_Simulation1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meteorological_system_simulation1;
+
+/**
+ *
+ * @author USER
+ */
+public class Meteorological_System_Simulation1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("----------------------------------------");
+        System.out.println("【ⓂⒺⓉⒺⓄⓇⓄⓁⓄⒼⒾⒸⒶⓁ ⓈⓎⓈⓉⒺⓂ】 ");
+        System.out.println("---------------------------------------- \n");
+        System.out.println("If you are planning to do a road trip in "
+                + "Simon Bolívar Avenue");
+        System.out.println("You must check some meteorological "
+                + "conditions to avoid a crash \n");
+
+    }
+    
+}
