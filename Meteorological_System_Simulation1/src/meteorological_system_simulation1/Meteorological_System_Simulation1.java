a6a2cb77f1a4bce6b0ff47c52e4741109990e7d3
FerBacuy <dia_nara15@hotmail.com>
2021-02-28 15:22:52 -0500

    Arrays

--- Meteorological_System_Simulation1/src/meteorological_system_simulation1/Meteorological_System_Simulation1.java
+++ Meteorological_System_Simulation1/src/meteorological_system_simulation1/Meteorological_System_Simulation1.java
@@ -8,13 +8,15 @@ public class Meteorological_System_Simulation1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         boolean mainLoop = true;
         int option;
         do {
             System.out.println("❶ ➙ METEOROLOGICAL SYSTEM ");
             System.out.println("❷ ➙ GEOESPATIAL CALCULATOR");
             System.out.println("❸ ➙ TEMPERATURE CONVERSION");
-            System.out.println("❹ ➙ EXIT");
+            System.out.println("❹ ➙ WEATHER ARRAYS");
+            System.out.println("❺  ➙ EXIT");
             System.out.printf("Enter your menu option --> ");
             option = scanner.nextInt();
             switch (option) {
@@ -261,7 +263,126 @@ public class Meteorological_System_Simulation1 {
 
                     showConvertionTemperature(scanner);
                     break;
-                case 4:
+case 4:
+                    int count = 0;
+                    int sum = 0;
+                    double average;
+                    System.out.println("1 ➙ JANUARY ");
+                    System.out.println("2 ➙ FEBRUARY");
+                    System.out.println("3 ➙ MARCH");
+                    System.out.println("4 ➙ APRIL");
+                    System.out.println("5 ➙ MAY");
+                    System.out.println("6 ➙ JUNE");
+                    System.out.println("7 ➙ JULY");
+                    System.out.println("8 ➙ AGOST");
+                    System.out.println("9 ➙ SEPTEMBER");
+                    System.out.println("10 ➙ OCTOBER");
+                    System.out.println("11 ➙ NOVEMBER");
+                    System.out.println("12 ➙ DECEMBER");
+                    System.out.println("13 ➙ Exit ✘ \n");
+                    System.out.printf("Enter your menu option --> ");
+                    option = scanner.nextInt();
+                    System.out.print("Enter 1 to 4 weeks to be averaged : ");
+                    int weeks = scanner.nextInt();
+                    int[] temperatures = new int[weeks];
+                    switch (option) {
+                        case 1:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 2:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 3:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 4:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 5:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 6:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 7:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 8:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 9:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 10:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 11:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 12:
+                            sum = readTemperature(weeks, temperatures, input, sum);
+                            average = operatorAverage(sum, weeks);
+                            count = readWeeks(weeks, temperatures, average, count);
+                            calculateRain(average, count);
+                            break;
+
+                        case 13:
+                            System.out.println("IT HAS BEEN A PLEASURE TO HELP YOU ( ＾◡＾)"
+                                    + "COME BACK SOON...");
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("ERROR ✘ \n");
+                            System.exit(0);
+                            break;
+
+                    }
+
+                case 5:
                     System.out.println("IT HAS BEEN A PLEASURE TO HELP YOU ( ＾◡＾)"
                             + "COME BACK SOON...");
                     System.exit(0);
@@ -380,6 +501,37 @@ public class Meteorological_System_Simulation1 {
         System.out.printf("The degrees in celsius are;" + c);
         System.out.printf("\n Kelvin degrees are: " + k);
         System.out.println();
+    }
+
+    private static void calculateRain(double average, int count) {
+        System.out.printf("Average temp = %.1f\n", average);
+        if (average <= 20) {
+            System.out.println("There is a 95% probability of rain that week.");
+        }
+        System.out.println(count + " weeks above average");
+    }
+
+    private static int readTemperature(int weeks, int[] temperatures, Scanner input, int sum) {
+        for (int i = 0; i < weeks; i++) {
+            System.out.print("Week " + (i + 1) + "'s low temperature in °C: ");
+            temperatures[i] = input.nextInt();
+            sum += temperatures[i];
+        }
+        return sum;
+    }
+
+    private static double operatorAverage(int sum, int weeks) {
+        double average = (double) sum / weeks;
+        return average;
+    }
+
+    private static int readWeeks(int weeks, int[] temperatures, double average, int count) {
+        for (int i = 0; i < weeks; i++) {
+            if (temperatures[i] > average) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
